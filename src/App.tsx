f9a172d5061f8faf018e0a39c742294bbb9465e4
RVargas <randall.vargas@iica.int>
2023-05-23 14:48:47 -0600

    Agrego botón de VR

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Suspense, useState } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { XR } from '@react-three/xr'
+import { Controllers, VRButton, XR } from '@react-three/xr'
 import { Preload, OrbitControls } from '@react-three/drei'
 import './App.scss';
 import { Portal } from './Components/Three/Portal';
@@ -13,8 +13,10 @@ function App() {
   return (
     <>
       <Layout>
+        <VRButton />
         <Canvas style={{zIndex:0}} frameloop="always" camera={{ fov: 70, position: [0, 0, 0.1], rotation: [0, 0, 0] }}>
           <XR>
+            <Controllers rayMaterial={{color: 'blue'}} hideRaysOnBlur={false}/>
             <OrbitControls enableZoom={true} enablePan={false} enableDamping dampingFactor={0.2} autoRotate={false} rotateSpeed={-0.5} />
             <Suspense fallback={null}>
               <Preload all />
